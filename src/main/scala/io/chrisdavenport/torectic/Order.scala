9337f85acba31595034a9fae0519fb558054080f
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-02-06 22:30:47 -0500

    Add ParialOrdered

--- src/main/scala/io/chrisdavenport/torectic/Order.scala
+++ src/main/scala/io/chrisdavenport/torectic/Order.scala
@@ -1,8 +1,13 @@
 package io.chrisdavenport.torectic
 
 trait Order[A]{
-    def equality : Equality[A] = new Equality[A] {
-        override def equal(x: A, y: A): Boolean = compare(x,y) == Order.EQ
+    def equality : Equality[A] = partialOrder.equality
+    def partialOrder: PartialOrder[A] = new PartialOrder[A]{
+        override def partialCompare(x: A, y: A): PartialOrder.PartialOrdered = compare(x,y) match {
+            case Order.EQ => PartialOrder.PartialEQ
+            case Order.LT => PartialOrder.PartialLT
+            case Order.GT => PartialOrder.PartialGT
+        }
     }
     def compare(x: A, y: A): Order.Ordered
 }
@@ -12,6 +17,16 @@ object Order {
     def apply[A](implicit ev: Order[A]): Order[A] = ev
 
     sealed trait Ordered
+    object Ordered {
+        implicit val orderEquality: Equality[Ordered] = new Equality[Ordered]{
+            override def equal(x: Ordered, y: Ordered): Boolean = (x, y) match {
+                case (EQ, EQ) => true
+                case (LT, LT) => true
+                case (GT, GT) => true
+                case _ => false
+            }
+        }
+    }
     case object EQ extends Ordered
     case object LT extends Ordered
     case object GT extends Ordered
---
+++ src/main/scala/io/chrisdavenport/torectic/PartialOrder.scala
@@ -0,0 +1,28 @@
+package io.chrisdavenport.torectic
+
+trait PartialOrder[A]{
+    def equality : Equality[A] = new Equality[A] {
+        override def equal(x: A, y: A): Boolean = PartialOrder.PartialOrdered.partialOrderedEq.equal(partialCompare(x,y), PartialOrder.PartialEQ)
+    }
+    def partialCompare(x: A, y: A): PartialOrder.PartialOrdered
+}
+
+object PartialOrder {
+    sealed trait PartialOrdered
+    object PartialOrdered {
+        implicit val partialOrderedEq: Equality[PartialOrdered] = new Equality[PartialOrdered]{
+            override def equal(x: PartialOrdered, y: PartialOrdered): Boolean = (x,y) match {
+                case (CannotCompare, CannotCompare) => true
+                case (PartialEQ, PartialEQ) => true
+                case (PartialGT, PartialGT) => true
+                case (PartialLT, PartialLT) => true
+                case _ => false
+            }
+        }
+    }
+    case object CannotCompare extends PartialOrdered
+    case object PartialEQ extends PartialOrdered
+    case object PartialGT extends PartialOrdered
+    case object PartialLT extends PartialOrdered
+
+}
